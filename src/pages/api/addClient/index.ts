48855fbd3da3cf9d6a4d8d6d6ed4c17ad934b547
Kadson Lima <kadson-kaio@hotmail.com>
2023-07-13 22:22:31 -0300

    feat: HomePage

--- src/pages/api/addClient/index.ts
+++ src/pages/api/addClient/index.ts
@@ -1,4 +1,3 @@
-import { doc } from "../../../credentials/googlesheets";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 interface DataClient {
@@ -15,45 +14,7 @@ export default async function handler(
 ) {
 
   return res.status(200)
-  await doc.loadInfo();
 
-  console.log(req.body);
-  
-
-  const { nome, contato, server, service, description }: any = req.body.data;
-
-  if (req.method === 'POST') {
-    const servicosString = Object.entries(service)
-    .filter(([_, value]) => value === true)
-    .map(([key, _]) => key).join(", ");
-
-
-  const serverString = Object.entries(server)
-    .filter(([_, value]) => value === true)
-    .map(([key, _]) => key).join(", ");
-
-  // ACESSA A PRIMEIRA SHEET
-  const sheet = doc.sheetsByIndex[0];
-
-  const row = await sheet.addRow({
-    nome,
-    contato,
-    server:serverString,
-    service:servicosString,
-    description,
-  });
-
-  if (row) {
-    return res.status(200).json({ message: "Pedido Feito!" });
-  }
-
-  res.status(400).json({ message: "Pedido Não pode ser Feito!" });
-    
-  
-  } else {
-    // Rejeita outras requisições (GET, PUT, DELETE, etc.)
-    res.status(405).json({ message: 'Método não permitido' });
-  }
 
 
 }
